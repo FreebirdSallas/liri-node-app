3a3288cdfe4338205ff8ea1a83174c0a57e939a0
Dawn Sallas <dawndesignerd@Dawns-MBP-2.hsd1.va.comcast.net>
2019-10-02 17:54:32 -0400

    Now have spodify song search working

--- liri.js
+++ liri.js
@@ -1,46 +1,73 @@
 require("dotenv").config();
-
+var request = require("request");
 var keys = require("./keys.js");
-var Spotify = require("node-spotify-api")
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
 
 
-var runAction = function(func, parm) {
-    switch (func) {
-        case "concert-this":
-            concertThis(parm)
-            break
-        case "spotify-this-song":
-            spotifyThisSong(parm)
-            break
-        case "movie-this":
-            movieThis(parm)
-            break
-        case "do-what-it-says":
-            doWhatItSays()
-            break
-        default:
-            outputData("That is not a command that I recognize, please try again.") 
-    }
+var action = process.argv[2];
+var value = process.argv[3];
+
+switch (action) {
+    case "concert-this":
+        concertThis(value);
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong(value);
+        break;
+
+    case "movie-this":
+        movieThis(value);
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays(value);
+        break;
+
+    default:
+        outputData("That is not a command that I recognize, please try again.")
+}
+console.log("searching for: " + action);
+
+// movie function
+function movieThis(value) {
+
+    var queryUrl = "http://www.omdbapi.com/?s=" + value + "&y=&plot=short&apikey=43f7938d";
+
+    request(queryUrl, function (err, response, body) {
+        if (!value) {
+            value = "Mr Nobody";
+        }
+
+        var results = JSON.parse(body);
+
+        if (!err && response.statusCode === 200) {
+            console.log("Title: " + results.Tile);
+            console.log("Release Year: " + results.Year);
+        }
+
+    })
 }
 
-var spotifyThisSong = function(song){
-    // Default should be "Gold on the cieling" by The Black Keys
-    if (!song){
-        song = "Gold On The Ceiling The Black Keys"
+// song function
+function spotifyThisSong(value) {
+
+    var spotify = new Spotify(keys.spotify);
+
+    if (!value) {
+        value = "Gold On The Ceiling"
     }
 
-var spotify = new Spotify(keys.spotify);
-
-spotify.search({ type: "track", query: song, limit: 1}, function(err, result) {
-
-    // If there is an error log it.
-    if (err) {
-      console.log(err);
-    }
-  
-    console.log(JSON.stringify(result, null, 2));
-  
-  });
+    spotify.search({ type: "track", query: value, limit: 1 }, function (err, data) {
+        // If there is an error log it.
+        if (err) {
+            console.log(err);
+        }
+        // console.log(data);
+        var songInfo = data.tracks.items;
+        console.log("Artist(s): " + songInfo[0].artists[0].name);
+        console.log("Song: " + songInfo[0].name);
+    });
 }
 
-runAction(process.argv[2],process.argv[3]);
